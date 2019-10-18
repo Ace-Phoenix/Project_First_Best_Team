453dd8c9fbebda761d1271b87240ea9f1bfaff17
Euan Barclay <Silverswimmer63@gmail.com>
2019-10-18 10:34:49 -0400

    asdf

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const content = `
 Jake is smart;false
 the earth is flat;false
@@ -62,30 +61,3 @@ gaming laptops are good;false
 Mike can do basic motor functions;false
 siege has small maps;true
 Mike is cool;false`
-=======
-/* stringParser(string)
-uses regex to find the end of each line cleans them and makes them an array
-students check https://beckism.com/2010/09/splitting-lines-javascript/ for regex
-you also want it clean off any lagging carage returns from the end of the line
-@param string: {string} a big old string to be read
-@return {array} the contents of the string an array, 1 line per array index;
-*/
-//
-/* arrayReader(array, split)
-take an array of strings in the format of text 1 split text 2
-cleans up any extra whitespace
-turns this into an object of form {str: text1, bool: bool based on text 2}
-text 2 is assumed to be the words true or false. Uses these to set bools
-makes a new array where each element is an object.
-@param array: {array} an array with string containing the split
-@param split: {string} the item to split at
-@return {array} an array of objects keyed with str and bool and negated : false
-*/
-
-/* stringReader(string, split=";")
-a wrapper function for stringParser and arrayReader
-@param string {string} a heaping long string covering many lines
-@pram split=";" {string} where to split the string
-@return {array} an array of objects keyed str and bool
-*/
->>>>>>> 7427c88498be2257f3e4a1da1064c77edbea5d8b
