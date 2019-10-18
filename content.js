b39afaca0a47851ca6d729fe33b45c443a607c14
Euan Barclay <Silverswimmer63@gmail.com>
2019-10-18 10:34:03 -0400

    Did text stuf

---
+++ content.js
@@ -0,0 +1,63 @@
+const content = `
+Jake is smart;false
+the earth is flat;false
+Jake can't not roast;false
+working in this class is helps;true
+pluto is not a normal planet;true
+global warming isn't real;false
+Bowen is not not not not not not smart;true
+Jake is a good driver;false
+the sun is hot;true
+Aidan is not lazy;false
+when working with Aidan little work gets done;true
+Mike isn't a total beast with monty in rainbow six siege;false
+pidgins were made by the government;true
+bad dragon is a comoto dragon breeding company with the goal of making them a domesticated pet;false
+the earth is not flat;true
+the earth is not a perfect sphere;true
+radiation kills;true
+Mike can spell much good;true
+global warming is bad;true
+Memphis = kim john un;false
+fire hurts;false
+Aidan = bad at siege;true
+Memphis is secretly tall but has prosthetic legs;false
+Aidan noob rages on rainbow 6 siege (and sucks at it);true
+Euan is Memphis's twin and took his legs;false
+it smells like stale diretos;false
+Emma is tall;false
+it is raining outside;false
+Aidan is an idiot;true
+Bowen is cool;true
+Memphis cant reach the top shelf;true
+LaMorie has baby legs;true
+Bowen has aidans wallet;false
+Mike cant spell refrigir8tors;true
+Mr MaLorie secretly works for nasa;false
+Jake has a shark neckless;true
+Jake like pizza;true
+it is still summer;false
+Jakes computer is a dcu;true
+Aidan has a youtube channel called blaze conroy;true
+sharks have arms;false
+Bowen doesnt have glasses;false
+Kade has a crush on brad;true
+Euan is swim;true
+Mike is trash;true
+Aidan is the better leader;false
+Aidan has a future;false
+tbb is best;false
+LaMorie is cool;true
+Mike is here legaly;true
+alienwear is a good brand;false
+the sky is blue;true
+LaMorie has baby legs;true
+Jake is a noob;true
+pc masterace;true
+Jake is a good driver;false
+Mike is not a squeaker;false
+Aidan has a wallet;false
+gaming laptops are good;false
+Mike can do basic motor functions;false
+siege has small maps;true
+Mike is cool;false`
